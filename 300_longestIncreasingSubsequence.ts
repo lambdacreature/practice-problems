fda19d013d7126abe562c70bb153db92f2ec9c33
lambdacreature <lambdacreature@gmail.com>
2026-07-02 11:12:00 -0400

    removed dumbass optimization on 300

--- 300_longestIncreasingSubsequence.ts
+++ 300_longestIncreasingSubsequence.ts
@@ -6,11 +6,7 @@ function lengthOfLIS(nums: number[]): number {
   let longestLength = 1;
   for (let i = 0; i < nums.length; i++) {
     dp.push(1);
-    for (let j = i; j >= 0; j--) {
-      if (j+2 < longestLength) {
-        break;
-      }
-
+    for (let j = 0; j < i; j++) {
       if (nums[j] < nums[i]) {
         dp[i] = Math.max(dp[j]+1, dp[i]);
       }
