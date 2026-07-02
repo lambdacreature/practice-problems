ae67fef608b5a24e6c7d1f38bcb2d7570f4f584e
lambdacreature <lambdacreature@gmail.com>
2026-07-02 10:19:25 -0400

    first attempt at LIS

---
+++ 300_longestIncreasingSubsequence.ts
@@ -0,0 +1,27 @@
+function lengthOfLIS(nums: number[]): number {
+  // dp[i] = tamaño de la LIS que tiene
+  // nums[i] como su último elemento
+  const dp: number[] = []
+
+  let longestLength = 1;
+  for (let i = 0; i < nums.length; i++) {
+    dp.push(1);
+    for (let j = i; j >= 0; j--) {
+      if (j+2 < longestLength) {
+        break;
+      }
+
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[j]+1, dp[i]);
+      }
+    }
+
+    longestLength = Math.max(longestLength, dp[i]);
+  } 
+
+  return longestLength;
+}
+
+const testNums: number[] = [10, 9, 2, 5, 3, 7, 101, 18];
+
+console.log(lengthOfLIS(testNums))
