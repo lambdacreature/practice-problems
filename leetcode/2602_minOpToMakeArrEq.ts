905b6f5cbd9644b527accd991b56c6acdfc651ad
lambdacreature <lambdacreature@gmail.com>
2026-08-17 15:26:31 +0000

    In javascript you cannot even sort an array of numbers, what a wonderful piece of shjt Uwu

--- leetcode/2602_minOpToMakeArrEq.ts
+++ leetcode/2602_minOpToMakeArrEq.ts
@@ -5,7 +5,7 @@ function minOperations(nums: number[], queries: number[]): number[] {
   // each entry in the answer array is
   const answer: number[] = [];
 
-  nums.sort();
+  nums.sort((a, b) => a - b);
 
   const prefixSum: number[] = [ nums[0] ];
   for (let i = 1; i < nums.length; i++) {
@@ -13,36 +13,36 @@ function minOperations(nums: number[], queries: number[]): number[] {
   }
 
   for (const query of queries) {
-    let lo = 0;
-    let hi = nums.length;
+    let low = 0;
+    let high = nums.length-1;
+    let lowerBound = nums.length;
 
-    // encuentra donde cabria query en nums para mantener nums ordenado
-    // mas especifico: encuentra el menor indice para el cual se cumple
-    // nums[i] >= query
-    let mid = 0;
-    while (lo < hi) {
-      mid = Math.floor((lo + hi)/2);
-      if (nums[mid] < query) {
-        lo = mid+1;
+    while (low <= high) {
+      const mid = low + Math.floor((high-low)/2);
+
+      if (nums[mid] >= query) {
+        lowerBound = mid;
+        high = mid-1;
       } else {
-        // nums[mid] >= query
-        hi = mid;
+        low = mid+1;
       }
     }
 
-    // lo == hi
-    // en la izquierda se nums de query
-    // y en la derecha de resta query de nums
+    let left = 0;
+    if (lowerBound > 0) {
+      const sum = prefixSum[lowerBound-1];
+      const elementCount = lowerBound;
+      left = query * elementCount - sum;
+    }
 
-    // lo == 0 means todos los nums son mayores o iguales a query
-    // osea, no hay lado izq
-    const left  = lo === 0 ? 0 : query * (lo) - prefixSum[lo-1];
+    let right = 0;
+    if (lowerBound < nums.length) {
+      const sum = prefixSum[nums.length-1] - (lowerBound === 0 ? 0 : prefixSum[lowerBound-1]);
+      const elementCount = nums.length - lowerBound;
+      right = sum - query * elementCount;
+    }
 
-    // lo === nums.length means todos los elementos son menores a query
-    // osea que no hay lado derech.
-    const right = lo === nums.length ? 0 : (prefixSum[nums.length-1] - prefixSum[lo-1]) - query * (nums.length - lo);
-
-    answer.push(left + right);
+    answer.push(left+right);
   }
 
   return answer;
